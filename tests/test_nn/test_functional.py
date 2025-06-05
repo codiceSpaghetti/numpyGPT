054acf64b84737ad286b8b4032a8f19605ff8985
codiceSpaghetti <alessio.ser29@gmail.com>
2025-06-05 09:09:01 +0200

    adapted functional tests on new cross entropy implementation

--- tests/test_nn/test_functional.py
+++ tests/test_nn/test_functional.py
@@ -1,4 +1,5 @@
 import unittest
+
 import numpy as np
 
 from numpyGPT.nn.functional import cross_entropy_loss
@@ -7,41 +8,40 @@ from numpyGPT.nn.functional import cross_entropy_loss
 class TestCrossEntropyLoss(unittest.TestCase):
 
     def test_cross_entropy_basic(self):
-        Y_hat = np.array([
+        probs = np.array([
             [0.7, 0.1, 0.1, 0.1],
             [0.1, 0.1, 0.7, 0.1],
             [0.1, 0.6, 0.2, 0.1]
         ])
+        logits = np.log(probs + 1e-8)
         Y_true = np.array([0, 2, 1])
 
-        loss = cross_entropy_loss(Y_hat, Y_true)
+        loss = cross_entropy_loss(logits, Y_true)
         expected = -(np.log(0.7) + np.log(0.7) + np.log(0.6)) / 3
 
         self.assertAlmostEqual(loss, expected, places=6)
         self.assertGreater(loss, 0)
 
     def test_cross_entropy_perfect(self):
-        Y_hat = np.array([
-            [1.0, 0.0, 0.0],
-            [0.0, 0.0, 1.0]
+        logits = np.array([
+            [10.0, -10.0, -10.0],
+            [-10.0, -10.0, 10.0]
         ])
         Y_true = np.array([0, 2])
 
-        loss = cross_entropy_loss(Y_hat, Y_true)
-        self.assertAlmostEqual(loss, 0.0, places=6)
+        loss = cross_entropy_loss(logits, Y_true)
+        self.assertAlmostEqual(loss, 0.0, places=2)
 
     def test_cross_entropy_numerical_stability(self):
-        eps = 1e-7
-        Y_hat = np.array([
-            [eps, 1.0-eps, 0.0],
-            [eps, 0.0, 1.0-eps]
+        logits = np.array([
+            [-15.0, 1.0, -10.0],
+            [-15.0, -10.0, 1.0]
         ])
         Y_true = np.array([0, 0])
 
-        loss = cross_entropy_loss(Y_hat, Y_true)
-        expected = -np.log(eps + eps)
+        loss = cross_entropy_loss(logits, Y_true)
 
-        self.assertAlmostEqual(loss, expected, places=6)
+        self.assertGreater(loss, 15.0)
         self.assertGreater(loss, 0)
 
 
